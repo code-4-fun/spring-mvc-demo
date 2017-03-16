bec18aa87754dadfd10054c0905b1b234c6cbf32
Nalawade, Devendra | Deven | ESD <devendra.nalawade@rakuten.com>
2017-03-16 16:06:44 +0900

    demo app

---
+++ src/main/java/com/example/spring/mvc/ProfileController.java
@@ -0,0 +1,19 @@
+package com.example.spring.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ProfileController {
+    
+    @RequestMapping(path="/time", method=RequestMethod.GET)
+    public ModelAndView showProfile(HttpServletRequest request, HttpServletResponse response) {
+        return new ModelAndView("time");
+    }
+
+}
